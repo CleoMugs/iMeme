39a7994f4f2925fcd614c58501be7a2515825eed
CleoMugs <cleophas.mugeni@gmail.com>
2020-08-01 15:33:48 +0300

    customized PostAdmin

--- iblog/admin.py
+++ iblog/admin.py
@@ -3,4 +3,10 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'author', 'slug', 'status', 'created_on')
+	list_filter = ("status",)
+	search_fields = ['title', 'content']
+	prepopulated_fields = {'slug':('title',)}
+
+admin.site.register(Post, PostAdmin)
