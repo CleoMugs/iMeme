b78daf29926ecb47a297b7333a7ebd3a5d235e5f
CleoMugs <cleophas.mugeni@gmail.com>
2020-08-01 16:33:51 +0300

    added base-template

--- iblog/views.py
+++ iblog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+	#model = Post
+	queryset = Post.objects.filter(status=1).order_by('created_on')
+	template_name = 'iblog/index.html'
+	#ordering = ['-created_on']
+
+
+class PostDetail(generic.DetailView):
+	model = Post
+	template_name = 'iblog/post_detail.html'
