c9bd113496b6a54f6ecbaf46a54729e73bfe9258
CleoMugs <cleophas.mugeni@gmail.com>
2020-08-01 21:17:44 +0300

    added navbar

--- iblog/views.py
+++ iblog/views.py
@@ -5,11 +5,11 @@ from .models import Post
 
 class PostList(generic.ListView):
 	#model = Post
-	queryset = Post.objects.filter(status=1).order_by('created_on')
-	template_name = 'iblog/index.html'
+	queryset = Post.objects.filter(status=1).order_by('-created_on')
+	template_name = 'index.html'
 	#ordering = ['-created_on']
 
 
 class PostDetail(generic.DetailView):
 	model = Post
-	template_name = 'iblog/post_detail.html'
+	template_name = 'post_detail.html'
