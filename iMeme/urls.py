c6554c6d36023e299b7578a68d9917ef6c1fe985
CleoMugs <cleophas.mugeni@gmail.com>
2020-08-02 21:50:18 +0300

    added django-summernote

--- iMeme/urls.py
+++ iMeme/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('iblog.urls')),
+    path('summernote/', include(django_summernote.urls)),
 ]
