eed51dbc235888a24e9e91d3988727e6b4902cc5
CleoMugs <cleophas.mugeni@gmail.com>
2020-08-02 14:21:21 +0300

    added post_detail view

--- iblog/views.py
+++ iblog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Post
+from .models import Post, Comment
+from .forms import CommentForm
+from django.shortcuts import render, get_object_or_404
 
 
 class PostList(generic.ListView):
@@ -9,7 +11,34 @@ class PostList(generic.ListView):
 	template_name = 'index.html'
 	#ordering = ['-created_on']
 
-
+'''
 class PostDetail(generic.DetailView):
 	model = Post
 	template_name = 'post_detail.html'
+'''
+
+def post_detail(request, slug):
+	template_name = 'post_detail.html'
+	post = get_object_or_404(Post, slug=slug)
+	comments = post.comments.filter(active=True)
+
+	new_comment = None
+
+	if request.method == 'POST':
+		comment_form = CommentForm(data=request.POST)
+		if comment_form.is_valid():
+
+			# Create Comment object but don't save to database yet
+			new_comment = comment_form.save(commit=False)
+			# Assign the current post to the comment
+			new_comment.post = post
+			# Save the comment to the database
+			new_comment.save()
+
+
+	else:
+		comment_form = CommentForm()
+
+
+	context = {'post':post, 'comments':comments, 'new_comment':new_comment, 'comment_form':comment_form}
+	return render(request, template_name, context)	
