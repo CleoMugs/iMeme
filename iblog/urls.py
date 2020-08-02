5136757649079752132d3a934efc4e6229b63cf2
CleoMugs <cleophas.mugeni@gmail.com>
2020-08-02 16:41:05 +0300

    added pagination

--- iblog/urls.py
+++ iblog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 urlpatterns =[
 	path('', views.PostList.as_view(), name='home'),
+	#path('', views.CommentList.as_view(), name='comment-count'),
 	#path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 	path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- iblog/views.py
+++ iblog/views.py
@@ -6,16 +6,20 @@ from django.shortcuts import render, get_object_or_404
 
 
 class PostList(generic.ListView):
-	#model = Post
 	queryset = Post.objects.filter(status=1).order_by('-created_on')
 	template_name = 'index.html'
-	#ordering = ['-created_on']
+	paginate_by = 2
+'''
+class CommentList(generic.ListView):
+	queryset = Comment.objects.count()
+	template_name = 'index.html'	
 
 '''
+
 class PostDetail(generic.DetailView):
 	model = Post
 	template_name = 'post_detail.html'
-'''
+
 
 def post_detail(request, slug):
 	template_name = 'post_detail.html'
