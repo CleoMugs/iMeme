9a65e7016ada685aa34adbba22d25ef1f63e22c4
CleoMugs <cleophas.mugeni@gmail.com>
2020-08-11 17:14:39 +0300

    added like-button

--- iblog/models.py
+++ iblog/models.py
@@ -47,6 +47,15 @@ class Comment(models.Model):
 		return 'Comment {self.body} by {self.name}'
 
 
+class Like(models.Model):
+	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+	liked_on = models.DateTimeField(auto_now_add=True, null=True)
+
+	def __str__(self):
+		return self.id
+
+
 class Profile(models.Model):
 	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
 	real_name =  models.CharField(max_length=200, null=True, blank=True)
