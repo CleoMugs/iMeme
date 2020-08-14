cc8dce07fa555dacefd762e22b15749b1be640e9
CleoMugs <cleophas.mugeni@gmail.com>
2020-08-14 14:12:02 +0300

    social-accounts added

--- iMeme/urls.py
+++ iMeme/urls.py
@@ -17,9 +17,13 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+
     path('', include('iblog.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('api-auth/', include('rest_framework.urls')),
