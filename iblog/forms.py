a104f29cc0a9c08cfcfebd9518ff271b20baa58d
CleoMugs <cleophas.mugeni@gmail.com>
2020-08-17 12:29:32 +0300

    redesigned login-page

--- iblog/forms.py
+++ iblog/forms.py
@@ -7,10 +7,41 @@ from django.core.exceptions import ValidationError
 
 
 class CreateUserForm(UserCreationForm):
-	username = forms.CharField(min_length=4, max_length=150)
+	username = forms.CharField(min_length=4, max_length=150, widget=forms.TextInput(attrs={'placeholder':'Username...'}))
+	email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder':'Email...'}))
+	password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Enter Password...'}))
+	password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Confirm Password...'}))
+
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
+
+
+
+class UserLoginForm(forms.ModelForm):
+	username = forms.CharField(min_length=4, max_length=150, widget=forms.TextInput(attrs={'placeholder':'Username...'}))
+	password = forms.CharField(widget=forms.PasswordInput())
+	class Meta:
+		model = User
+		fields = ['username', 'password']
+
+
+class CommentForm(forms.ModelForm):
+	class Meta:
+		model = Comment
+		fields = ('name', 'email', 'body')
+
+
+class UserProfileForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['real_name', 'profile_pic', 'location', 'occupation']
+
+
+class EditProfileForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['real_name', 'location', 'occupation', 'profile_pic']
 
 
 '''
@@ -45,27 +76,3 @@ class CreateUserForm(UserCreationForm):
 		return password2
 
 '''
-
-class UserLoginForm(forms.ModelForm):
-	#password = forms.CharField(widget=forms.PasswordInput())
-	class Meta:
-		model = User
-		fields = ['username', 'password']
-
-
-class CommentForm(forms.ModelForm):
-	class Meta:
-		model = Comment
-		fields = ('name', 'email', 'body')
-
-
-class UserProfileForm(forms.ModelForm):
-	class Meta:
-		model = Profile
-		fields = ['real_name', 'profile_pic', 'location', 'occupation']
-
-
-class EditProfileForm(forms.ModelForm):
-	class Meta:
-		model = Profile
-		fields = ['real_name', 'location', 'occupation', 'profile_pic']
