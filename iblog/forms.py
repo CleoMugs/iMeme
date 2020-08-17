9bf9eb09768f6c8d19f34c9cbeaf09ab492036e2
CleoMugs <cleophas.mugeni@gmail.com>
2020-08-17 11:58:59 +0300

    register form added

--- iblog/forms.py
+++ iblog/forms.py
@@ -3,15 +3,51 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Comment, Profile
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
 
 class CreateUserForm(UserCreationForm):
+	username = forms.CharField(min_length=4, max_length=150)
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
 
 
+'''
+class CreateUserForm(UserCreationForm):
+	username = forms.CharField(label='Enter Username', min_length=4, max_length=150)
+	email = forms.EmailField(label='Enter email')
+	password1 = forms.CharField(label='Enter password', widget=forms.PasswordInput)
+	password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+	model = User
+	def clean_username(self):
+		username = self.cleaned_data['username'].lower()
+		r = User.objects.filter(username=username)
+		if r.count():
+		    raise  ValidationError("Username already exists")
+		return username
+
+	def clean_email(self):
+		email = self.cleaned_data['email'].lower()
+		r = User.objects.filter(email=email)
+		if r.count():
+		    raise  ValidationError("Email already exists")
+		return email
+
+	def clean_password2(self):
+		password1 = self.cleaned_data.get('password1')
+		password2 = self.cleaned_data.get('password2')
+
+		if password1 and password2 and password1 != password2:
+		    raise ValidationError("Password don't match")
+
+		return password2
+
+'''
+
 class UserLoginForm(forms.ModelForm):
-	password = forms.CharField(widget=forms.PasswordInput())
+	#password = forms.CharField(widget=forms.PasswordInput())
 	class Meta:
 		model = User
 		fields = ['username', 'password']
