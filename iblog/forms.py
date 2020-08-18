2b41831439987f09da9bcf0b3afe7b2bf12c6aad
CleoMugs <cleophas.mugeni@gmail.com>
2020-08-18 20:40:53 +0300

    fixed sitemaps

--- iblog/forms.py
+++ iblog/forms.py
@@ -63,35 +63,3 @@ class EditProfileForm(forms.ModelForm):
 		fields = ['real_name', 'location', 'occupation', 'profile_pic']
 
 
-'''
-class CreateUserForm(UserCreationForm):
-	username = forms.CharField(label='Enter Username', min_length=4, max_length=150)
-	email = forms.EmailField(label='Enter email')
-	password1 = forms.CharField(label='Enter password', widget=forms.PasswordInput)
-	password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
-
-	model = User
-	def clean_username(self):
-		username = self.cleaned_data['username'].lower()
-		r = User.objects.filter(username=username)
-		if r.count():
-		    raise  ValidationError("Username already exists")
-		return username
-
-	def clean_email(self):
-		email = self.cleaned_data['email'].lower()
-		r = User.objects.filter(email=email)
-		if r.count():
-		    raise  ValidationError("Email already exists")
-		return email
-
-	def clean_password2(self):
-		password1 = self.cleaned_data.get('password1')
-		password2 = self.cleaned_data.get('password2')
-
-		if password1 and password2 and password1 != password2:
-		    raise ValidationError("Password don't match")
-
-		return password2
-
-'''
