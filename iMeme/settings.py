4599df9568dfc32e760ed40a53201ffa0338586d
CleoMugs <cleophas.mugeni@gmail.com>
2020-09-13 21:47:43 +0300

    postgresql

--- iMeme/settings.py
+++ iMeme/settings.py
@@ -116,25 +116,8 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 WSGI_APPLICATION = 'iMeme.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blog',
-        'USER':'mugs',
-        'PASSWORD':'kakosh123',
-        'HOST': 'localhost',
-        'PORT': 5432,
-        
-    }
-}
-'''
-
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-
 
 
 # Password validation
